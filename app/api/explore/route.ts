4fc45d749b27bda02c18c78a102b0555066c3226
Lyonel Pierce <lyonel@live.com>
2023-09-11 21:18:33 -0400

    home

--- app/api/explore/route.ts
+++ app/api/explore/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getFeed, getHomeFeed } from "@/lib/feed";
+import { getHomeFeed } from "@/lib/feed";
 
 export const revalidate = 0;
 
@@ -7,21 +7,11 @@ export async function GET(req: Request) {
   const url = new URL(req.url);
   const pathname = url.pathname;
 
-  if (pathname === "/") {
-    try {
-      const homeFeed = await getHomeFeed();
-      return NextResponse.json(homeFeed);
-    } catch (error) {
-      console.log(error);
-      return new NextResponse("Internal error.", { status: 500 });
-    }
-  } else {
-    try {
-      const galleryResponse = await getFeed();
-      return new NextResponse(JSON.stringify(galleryResponse), { status: 200 });
-    } catch (error) {
-      console.log(error);
-      return new NextResponse("Internal error.", { status: 500 });
-    }
+  try {
+    const homeFeed = await getHomeFeed();
+    return NextResponse.json(homeFeed);
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Internal error.", { status: 500 });
   }
 }
